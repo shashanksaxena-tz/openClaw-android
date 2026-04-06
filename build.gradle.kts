9cb36d7d7dd4679ddd675223af6406ae14ed2025
Shashank Saxena <shashank.saxena@taazaa.com>
2026-04-06 19:04:00 +0530

    Fix: Use KSP 2.3.6 to match Gallery (new KSP versioning format for Kotlin 2.2.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
-    id("com.google.devtools.ksp") version "2.2.0-1.0.20" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
 }
